f8cc756dc695bfe9b5b3012851d2626ef1d129b0
melanie.opixido <melanie.terzic@gmail.com>
2021-03-31 12:19:35 +0200

    fix: add plugins

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,23 +4,16 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-smoothing: antialiased;
   box-sizing: border-box;
 }
 
