46e56d324ed59124cd5d68f9eeb77f11a5699f04
Rémy Dumas <remy.dumas@edu.gobelins.fr>
2021-06-21 17:00:33 +0200

    :bug: Update bug sound loading

--- components/organisms/sound.vue
+++ components/organisms/sound.vue
@@ -59,11 +59,13 @@ export default {
     this.$nextTick(()=>{
       console.log(this.$props.options.sound.name);
       this.$data.sound.src = require(`~/assets/sounds/${this.$route.name === "index" ? "chapter-1" : this.$route.name}/${this.$props.options.sound.name}.mp3`).default;
+      
       // this.$data.sound.audio = new Audio(this.$data.sound.src); 
       // this.$store.commit('addRessource', 'ok');
       // this.$data.sound.audio.addEventListener('canplaythrough', () => {
       //   this.$store.commit('removeRessource', this.$el);
       // });
+
       if (this.$props.options.sound.loop) {
         this.$data.sound.audio.addEventListener('ended', function () {
           this.$data.sound.audio.currentTime = 0;
